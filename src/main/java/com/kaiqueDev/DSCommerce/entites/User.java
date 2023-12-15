f519a5ea7e79ed82ca6f9e6fb620fa3eafbc2103
kaiquesilvadev <j.kaique1570@gmail.com>
2023-12-15 19:46:58 -0300

    criação da entidade user

---
+++ src/main/java/com/kaiqueDev/DSCommerce/entites/User.java
@@ -0,0 +1,37 @@
+package com.kaiqueDev.DSCommerce.entites;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+
+}
