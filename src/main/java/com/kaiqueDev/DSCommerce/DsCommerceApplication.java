a6968eb5eeb056d6ad3754b1fb99ba90279c8c36
kaiquesilvadev <j.kaique1570@gmail.com>
2023-12-15 19:25:17 -0300

    first commit

---
+++ src/main/java/com/kaiqueDev/DSCommerce/DsCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kaiqueDev.DSCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaiqueDev/DSCommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiqueDev.DSCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
