e3934c042b5b4c041e109d1ec55abdbd65983c5e
kaiquesilvadev <j.kaique1570@gmail.com>
2023-12-15 21:05:05 -0300

    criação da entidade product

---
+++ src/main/java/com/kaiqueDev/DSCommerce/entites/Product.java
@@ -0,0 +1,36 @@
+package com.kaiqueDev.DSCommerce.entites;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+}
